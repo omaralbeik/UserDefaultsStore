ad9e8fe2bdf0751fbf3cdd2291229ebfe05620ad
Omar Albeik <omaralbeik@teknasyon.com>
2018-09-11 12:13:39 +0300

    feat: Add single store

---
+++ Sources/SingleUserDefaultsStore.swift
@@ -0,0 +1,83 @@
+//
+//  SingleUserDefaultsStore
+//
+//  Copyright (c) 2018-Present Omar Albeik - https://github.com/omaralbeik
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import Foundation
+
+/// SingleUserDefaults Store.
+open class SingleUserDefaultsStore<T: Codable & Identifiable> {
+
+	/// Store's unique identifier.
+	/// **Warning**: Never use the same identifier for two -or more- different stores.
+	public let uniqueIdentifier: String
+
+	/// JSON encoder. _(default is JSONEncoder())_
+	open var encoder = JSONEncoder()
+
+	/// JSON decoder. _(default is JSONDecoder())_
+	open var decoder = JSONDecoder()
+
+	/// UserDefaults store.
+	private var store: UserDefaults
+
+	/// Initialize store with given identifier.
+	/// **Warning**: Never use the same identifier for two -or more- different stores.
+	///
+	/// - Parameter uniqueIdentifier: store's unique identifier.
+	required public init?(uniqueIdentifier: String) {
+		guard let store = UserDefaults(suiteName: uniqueIdentifier) else { return nil }
+		self.uniqueIdentifier = uniqueIdentifier
+		self.store = store
+	}
+
+	/// Save object to store. _O(1)_
+	///
+	/// - Parameter object: object to save.
+	/// - Throws: JSON encoding error.
+	public func save(_ object: T) throws {
+		let data = try encoder.encode(object)
+		store.set(data, forKey: key)
+	}
+
+	/// Get object from store. _O(1)_
+	public var object: T? {
+		guard let data = store.data(forKey: key) else { return nil }
+		return try? decoder.decode(T.self, from: data)
+	}
+
+	/// Delete object from store. _O(1)_
+	public func delete() {
+		store.removeObject(forKey: key)
+		store.removeSuite(named: uniqueIdentifier)
+	}
+
+}
+
+// MARK: - Keys
+private extension SingleUserDefaultsStore {
+
+	/// store key for object.
+	var key: String {
+		return "\(uniqueIdentifier)-object"
+	}
+
+}
---
+++ Tests/SingleStoreTests.swift
@@ -0,0 +1,78 @@
+//
+//  UserDefaultsStore
+//
+//  Copyright (c) 2018-Present Omar Albeik - https://github.com/omaralbeik
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+//  THE SOFTWARE.
+
+import XCTest
+@testable import UserDefaultsStore
+
+final class SingleStoreTests: XCTestCase {
+
+	func testCreateStore() {
+		let store = createFreshUsersStore()
+		XCTAssertNotNil(store)
+	}
+
+	func testSaveObject() {
+		let store = createFreshUsersStore()!
+
+		XCTAssertNoThrow(try store.save(TestUser.john))
+		XCTAssertNotNil(store.object)
+		XCTAssertEqual(store.object!, TestUser.john)
+	}
+
+	func testSaveInvalidObject() {
+		let store = createFreshUsersStore()!
+
+		let user = TestUser(userId: 5, firstName: "firstName", lastName: "lastName", age: .nan)
+		XCTAssertThrowsError(try store.save(user))
+	}
+
+	func testObject() {
+		let store = createFreshUsersStore()!
+
+		XCTAssertNoThrow(try store.save(TestUser.johnson))
+		XCTAssertNotNil(store.object)
+	}
+
+	func testDeleteObject() {
+		let store = createFreshUsersStore()!
+
+		XCTAssertNoThrow(try store.save(TestUser.james))
+		XCTAssertNotNil(store.object)
+		XCTAssertEqual(store.object!, TestUser.james)
+
+		store.delete()
+		XCTAssertNil(store.object)
+	}
+
+}
+
+// MARK: - Helpers
+private extension SingleStoreTests {
+
+	func createFreshUsersStore() -> SingleUserDefaultsStore<TestUser>? {
+		let store = SingleUserDefaultsStore<TestUser>(uniqueIdentifier: "user")
+		store?.delete()
+		return store
+	}
+
+}
